ebfbfb82835854b2bdf56798be1e982936b5d4d5
Dylan Shah <dylan.shah@outlook.com>
2025-12-10 21:57:53 -0700

    LICENCE

--- include/PinIO.hpp
+++ include/PinIO.hpp
@@ -1,4 +1,28 @@
 #pragma once
+
+/**
+ * @file PinIO.hpp
+ * @author Dylan Shah (dylan.shah@outlook.com)
+ * @brief Wraps arduino pin functions into C++ classes
+ * @version 0.1
+ * @date 2025-12-10
+ *
+ * @copyright Copyright Ⓒ 2025
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <Arduino.h>
 
 class Pin {
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,38 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+/**
+ * @file main.cpp
+ * @author Dylan Shah (dylan.shah@outlook.com)
+ * @brief Use an ESP32 to play music
+ * @version 0.1
+ * @date 2025-12-10
+ *
+ * @copyright Copyright Ⓒ 2025
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
-
 #include "Chord Player.hpp"
 #include <Arduino.h>
 #include <EasyButton.h>
 
-// Set LED_BUILTIN if it is not defined by the Arduino framework
-// #define LED_BUILTIN 13
-
 void setup() {
     Serial.begin(115200);
-    Serial.println("Chord Player Test");
+    Serial.println(
+        "Copyright (C) 2025  Dylan Shah \n "
+        "This program comes with ABSOLUTELY NO WARRANTY; see "
+        "LICENSE for details.This is free software, and you are "
+        "welcome to redistribute it under certain conditions. Please see the "
+        "GNU General Public License 3.0 or later for details.");
+
     Chord_Player<5> chordPlayer({ 15, 4, 18, 21, 23 });
     Chord_Player<5>::Chord chord{
         { Chord_Player<5>::Chord::Note{ PWMPin::Note::C, 4 },
